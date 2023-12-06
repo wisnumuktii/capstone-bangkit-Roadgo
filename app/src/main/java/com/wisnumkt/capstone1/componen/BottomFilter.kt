61ac01a8b9f661270547ab17243988cea51c3d86
wisnumuktii <126446866+wisnumuktii@users.noreply.github.com>
2023-12-06 16:56:17 +0700

    post file

---
+++ app/src/main/java/com/wisnumkt/capstone1/componen/BottomFilter.kt
@@ -0,0 +1,22 @@
+package com.wisnumkt.capstone1.componen
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.TabRow
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.tooling.preview.Preview
+import com.wisnumkt.capstone1.ui.theme.Capstone1Theme
+
+@Composable
+fun BottomFilter() {
+
+}
+
+@Preview (showBackground = true)
+@Composable
+fun BottomFilterPreview() {
+    Capstone1Theme {
+        BottomFilter()
+    }
+}
